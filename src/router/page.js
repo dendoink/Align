f003349289f0481afbcd0a2d3839050657862272
dendionk <dendise7en@gmail.com>
2018-12-07 23:15:16 +0800

    AutoUpdate:[Friday, December 7th 2018, 11:14:15 pm]

--- src/router/page.js
+++ src/router/page.js
@@ -140,6 +140,26 @@ let pageroutes = [
     component: () => import('../posts/2018-12-07-Translate-2.md')
   },
   {
+    path: "/Site/2018-12-07-Translate-3",
+    component: () => import('../posts/2018-12-07-Translate-3.md')
+  },
+  {
+    path: "/post/2018-12-07-Translate-3",
+    component: () => import('../posts/2018-12-07-Translate-3.md')
+  },
+  {
+    path: "/Tutorial/2018-12-07-Translate-3",
+    component: () => import('../posts/2018-12-07-Translate-3.md')
+  },
+  {
+    path: "/Chrome/2018-12-07-Translate-3",
+    component: () => import('../posts/2018-12-07-Translate-3.md')
+  },
+  {
+    path: "/Tools/2018-12-07-Translate-3",
+    component: () => import('../posts/2018-12-07-Translate-3.md')
+  },
+  {
     path: "/Site/2018-12-07-Translate",
     component: () => import('../posts/2018-12-07-Translate.md')
   },
